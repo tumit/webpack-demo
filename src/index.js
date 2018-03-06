cd6ab79b49e4a3619dcedf714c8226bcc647763d
tumit <tumit@live.com>
2018-03-06 14:12:53 +0700

    feat(out-management)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'กรุงเทพมหานคร'], ' ');
@@ -13,6 +15,11 @@ function component() {
   ngIcon.src = Icon;
 
   element.appendChild(ngIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
