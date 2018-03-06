b1852004b3c8a52d4e2f4ee168a40f5ff41ffa54
tumit <tumit@live.com>
2018-03-06 12:06:36 +0700

    feat(file-loader): add file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var ngIcon = new Image();
+  ngIcon.src = Icon;
+
+  element.appendChild(ngIcon);
 
   return element;
 }
